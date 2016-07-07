1ae9f96d9688ca38fd667d53239367bc3ef154f4
Supanut Apikulvanich <supanut.overload@gmail.com>
2016-07-07 23:43:49 +0700

    Copy

--- app.js
+++ app.js
@@ -26,3 +26,18 @@ function handleChange(event) {
 textInput.value = "ขอ ให้ มาเล่น กัน เยอะๆ นะครับ . . .";
 resultText = generateTag(textInput.value);
 renderText();
+
+
+var clipboard = new Clipboard('.btn');
+
+clipboard.on('success', function(e) {
+  console.info('Action:', e.action);
+  console.info('Text:', e.text);
+  console.info('Trigger:', e.trigger);
+  e.clearSelection();
+});
+
+clipboard.on('error', function(e) {
+  console.error('Action:', e.action);
+  console.error('Trigger:', e.trigger);
+});
