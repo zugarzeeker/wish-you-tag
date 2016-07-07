25774416fec4cf93e9908f91dc4322c64cb9b042
Supanut Apikulvanich <supanut.overload@gmail.com>
2016-07-07 23:06:11 +0700

    Can Generate Text

--- app.js
+++ app.js
@@ -2,11 +2,19 @@ var textTag = document.getElementById('text-tag');
 var textInput = document.getElementById('textInput');
 
 function generateTag(text) {
-  return text;
+  var t = text.split(' ');
+  var s = [];
+  s.push('┏┷ ┓');
+  for (var i = 0, len = t.length; i < len; i++) {
+    s.push('┃ ??? ┃'.replace('???', t[i]));
+  }
+  s.push('┗━ ┛');
+  console.log(s);
+  return s.join('<br>');
 }
 
 function renderText(text) {
-  textTag.innerHTML = generateTag(text);
+  textTag.innerHTML = text;
 }
 
 function handleChange(event) {
